3c6fa39b50f3de488e8fee268a0efb583adf7b82
AlexZ2Z <pletnev.sasa@gmail.com>
2023-11-05 17:24:48 +0500

    project2 (and -> or) fix

--- src/main/java/edu/project2/MazeGenerator.java
+++ src/main/java/edu/project2/MazeGenerator.java
@@ -34,7 +34,7 @@ public class MazeGenerator {
     private void initMaze() {
         for (int i = 0; i < maze.length; i++) {
             for (int j = 0; j < maze[0].length; j++) {
-                maze[i][j] = BOTTOM_BIT & LEFT_BIT & TOP_BIT & RIGHT_BIT;
+                maze[i][j] = BOTTOM_BIT | LEFT_BIT | TOP_BIT | RIGHT_BIT;
             }
         }
     }
