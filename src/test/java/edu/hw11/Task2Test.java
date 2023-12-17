37d1a1ce883a990aac3a27c85056c90f3b7d8919
AlexZ2Z <pletnev.sasa@gmail.com>
2023-12-17 16:35:45 +0300

    hw11 task2 super fix

--- src/test/java/edu/hw11/Task2Test.java
+++ src/test/java/edu/hw11/Task2Test.java
@@ -1,8 +1,8 @@
 package edu.hw11;
 
+import java.lang.reflect.InvocationTargetException;
 import net.bytebuddy.ByteBuddy;
-import net.bytebuddy.agent.ByteBuddyAgent;
-import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
 import net.bytebuddy.implementation.MethodDelegation;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -10,43 +10,36 @@ import static net.bytebuddy.matcher.ElementMatchers.named;
 
 class Task2Test {
 
-    static class ArithmeticUtils {
+    public static class ArithmeticUtils {
 
-        private ArithmeticUtils() {
-        }
-
-        public static int sum(int a, int b) {
+        public int sum(int a, int b) {
             return a + b;
         }
     }
 
-    static class NewArithmeticImpl {
+    public static class NewArithmeticImpl {
 
-        private NewArithmeticImpl() {
-
-        }
-
-        public static int sum(int a, int b) {
+        public int sum(int a, int b) {
             return a * b;
         }
     }
 
     @Test
-    void testChangeClassBehavior() {
+    void testChangeClassBehavior()
+        throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         var excepted = 16;
 
-        ByteBuddyAgent.install();
-            new ByteBuddy().redefine(ArithmeticUtils.class)
-                .method(named("sum"))
-                .intercept(MethodDelegation.to(NewArithmeticImpl.class))
-                .make()
-                .load(
-                    ClassLoader.getSystemClassLoader(),
-                    ClassReloadingStrategy.fromInstalledAgent()
-                )
-                .getLoaded();
+        ArithmeticUtils dynamicType = new ByteBuddy()
+            .subclass(ArithmeticUtils.class)
+            .method(named("sum"))
+            .intercept(MethodDelegation.to(new NewArithmeticImpl()))
+            .make()
+            .load(getClass().getClassLoader(), ClassLoadingStrategy.Default.WRAPPER)
+            .getLoaded()
+            .getDeclaredConstructor()
+            .newInstance();
 
-        var actual = ArithmeticUtils.sum(4, 4);
+        var actual = dynamicType.sum(4, 4);
 
         Assertions.assertEquals(excepted, actual);
 
