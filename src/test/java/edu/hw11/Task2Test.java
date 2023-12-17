9e54e1d7bbda8747356ad1f26784a2ea8c79c06a
AlexZ2Z <pletnev.sasa@gmail.com>
2023-12-17 16:10:39 +0300

    hw11 add -XX:+EnableDynamicAgentLoading to pox.xml

--- src/test/java/edu/hw11/Task2Test.java
+++ src/test/java/edu/hw11/Task2Test.java
@@ -4,14 +4,11 @@ import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.agent.ByteBuddyAgent;
 import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
 import net.bytebuddy.implementation.MethodDelegation;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 
 class Task2Test {
-    private static final Logger LOGGER = LogManager.getLogger();
 
     static class ArithmeticUtils {
 
@@ -38,9 +35,7 @@ class Task2Test {
     void testChangeClassBehavior() {
         var excepted = 16;
 
-        try {
-
-            ByteBuddyAgent.install();
+        ByteBuddyAgent.install();
             new ByteBuddy().redefine(ArithmeticUtils.class)
                 .method(named("sum"))
                 .intercept(MethodDelegation.to(NewArithmeticImpl.class))
@@ -50,9 +45,7 @@ class Task2Test {
                     ClassReloadingStrategy.fromInstalledAgent()
                 )
                 .getLoaded();
-        } catch (Throwable e) {
-            LOGGER.error(e);
-        }
+
         var actual = ArithmeticUtils.sum(4, 4);
 
         Assertions.assertEquals(excepted, actual);
