33fff58bb6415c3de2dd66cd40908c45ceba7f19
AlexZ2Z <pletnev.sasa@gmail.com>
2023-12-17 16:00:16 +0300

    hw11 task2 fix

--- src/test/java/edu/hw11/Task2Test.java
+++ src/test/java/edu/hw11/Task2Test.java
@@ -4,11 +4,14 @@ import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.agent.ByteBuddyAgent;
 import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
 import net.bytebuddy.implementation.MethodDelegation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 
 class Task2Test {
+    private static final Logger LOGGER = LogManager.getLogger();
 
     static class ArithmeticUtils {
 
@@ -35,17 +38,21 @@ class Task2Test {
     void testChangeClassBehavior() {
         var excepted = 16;
 
-        ByteBuddyAgent.install();
-        new ByteBuddy().redefine(ArithmeticUtils.class)
-            .method(named("sum"))
-            .intercept(MethodDelegation.to(NewArithmeticImpl.class))
-            .make()
-            .load(
-                ClassLoader.getSystemClassLoader(),
-                ClassReloadingStrategy.fromInstalledAgent()
-            )
-            .getLoaded();
+        try {
 
+            ByteBuddyAgent.install();
+            new ByteBuddy().redefine(ArithmeticUtils.class)
+                .method(named("sum"))
+                .intercept(MethodDelegation.to(NewArithmeticImpl.class))
+                .make()
+                .load(
+                    ClassLoader.getSystemClassLoader(),
+                    ClassReloadingStrategy.fromInstalledAgent()
+                )
+                .getLoaded();
+        } catch (Throwable e) {
+            LOGGER.error(e);
+        }
         var actual = ArithmeticUtils.sum(4, 4);
 
         Assertions.assertEquals(excepted, actual);
